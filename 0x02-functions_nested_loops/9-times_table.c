fd13e434bed041bec2949898099563dfff96d67c
Yohannes Terfa <lightshadow011@gmail.com>
2021-06-22 11:32:33 +0000

    task 9 done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints time table
+ *
+ * Return: (void)
+ *
+ */
+
+void times_table(void)
+{
+	int x, y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if (((x * y) / 10) != 0)
+				_putchar('0' + ((x * y) / 10));
+			else
+				_putchar(' ');
+			_putchar('0' + ((x * y) % 10));
+			if (x != 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
