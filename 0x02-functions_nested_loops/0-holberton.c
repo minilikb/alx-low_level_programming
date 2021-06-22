ae6601ae68359fe2dbfdc9714403f730bd2cb471
Yohannes Terfa <lightshadow011@gmail.com>
2021-06-22 07:04:30 +0000

    task 0 done

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print text
+ *
+ * Return: 0 for success
+ *
+ */
+
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+<h2>More on loops and Functions</h2>
