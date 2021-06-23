5d8425429a18fd7005e4d0e8e270d886b298ec8c
Yohannes Terfa <lightshadow011@gmail.com>
2021-06-23 06:15:53 -0700

    adv task 101 done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Print alphabet
+ *
+ * Return: <0) for success <1> for failure
+ *
+ */
+
+int main(void)
+{
+	int sum = 0;
+	for (int i = 3; i < 1024; i++)
+	{
+		if (((i % 3) == 0)|| ((i % 5) == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
