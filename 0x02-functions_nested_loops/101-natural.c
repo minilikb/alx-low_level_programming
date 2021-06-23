64874d6ebbef1461e08f98d9defb74cd0de46d79
Yohannes Terfa <lightshadow011@gmail.com>
2021-06-23 06:19:08 -0700

    adv task 101 refix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,9 +11,10 @@ int main(void)
 {
 	int sum = 0;
 	int i;
+
 	for (i = 3; i < 1024; i++)
 	{
-		if (((i % 3) == 0)|| ((i % 5) == 0))
+		if (((i % 3) == 0) || ((i % 5) == 0))
 		{
 			sum += i;
 		}
