2407cb23d39700b5e469a04071260ab859b09108
Yohannes Terfa <lightshadow011@gmail.com>
2021-06-23 06:26:13 -0700

    adv task 102 done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Print alphabet
+ *
+ * Return: <0) for success <1> for failure
+ *
+ */
+
+int main(void)
+{
+	int i; 
+	int t1 = 0; 
+	int t2 = 1;
+	int nextTerm = t1 + t2;
+
+	for (i = 1; i < 50; ++i)
+	{
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	printf("\n");
+	return (0);
+}
