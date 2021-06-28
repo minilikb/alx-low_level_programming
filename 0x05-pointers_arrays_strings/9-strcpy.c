c6cbec1c0abd9b0fa5b6336de5cd28e5c0126ae7
Yohannes Terfa <lightshadow011@gmail.com>
2021-06-28 19:53:22 +0000

    task 5-9 done refix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int count = 0;
+	int i;
 
-	while (src[i] != 0)
-	{
-		count++;
-		i++;
-	}
-	for (i = 0; i <= count; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i + 1] = 0;
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+			dest[i] = src[i];
+		while (i < n)
+			dest[i++] = '\0';
+
 	return (dest);
 }
