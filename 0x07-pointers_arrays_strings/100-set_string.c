dfbdbf963f2f3e6225735c13019a68a44c6e891e
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-05 20:38:47 +0000

    task 100 done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets a value of char to pointer
+ * @s: pointer memory area
+ * @to: pointer for char to fill
+ *
+ * Return: (void)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
