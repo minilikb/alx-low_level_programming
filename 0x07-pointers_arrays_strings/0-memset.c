15ba9297ae8518172666ba54851e9ad30fd5941f
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-05 15:15:06 +0000

    task 0 done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: pointer memory area
+ * b: byte to fill
+ *
+ * n: selector number
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		s[counter] = b;
+	}
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+<h2> Even More pointers and Multi dimensional Arrays - Project </h2>
