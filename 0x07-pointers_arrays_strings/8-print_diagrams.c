b2e454f3d9cef5792a7a9ce07154255cfcc749e2
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-05 20:09:44 +0000

    task 8 refix

---
+++ 0x07-pointers_arrays_strings/8-print_diagrams.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints a sum of a square matrix.
+ * @a: pointer to square matrix
+ * @size: size of square matrix
+ * Return: (void)
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int counter, i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (counter = 0; counter < (size * size) ; counter = counter + size + 1)
+		sum1 += a[counter];
+	for (i = 0; i <= (size * size) - size; i = i + size - 1)
+		sum2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
+}
