c35208c2a43078c9e870dcb123624d15e53f1acc
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-13 18:47:14 +0000

    task 1 done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strdup - duplicate a string in memory
+ * @str: the pointer to string 
+ *
+ * Return: pointer to allocated space
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int counter, size;
+
+	for (counter = 0; str[counter]; counter++)
+	{
+		size++;
+	}
+	counter = 0;
+	array = malloc(sizeof(char) * size);
+	if (array == NULL || size == 0)
+		return (NULL);
+	for (counter = 0; str[counter]; counter++)
+		array[counter] = str[counter];
+	array[size - 1] = '\0';
+	return (array);
+}
