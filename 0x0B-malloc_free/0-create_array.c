f25afb39132e4fc0eaf31007eec17e8f27c2c77a
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-13 18:34:26 +0000

    task 0 done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * create_array - creates an array of given size
+ * @size: arrat size
+ * @c: initial value
+ *
+ * Return: pointer to allocated space
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int counter;
+
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return NULL;
+	for (counter = 0; counter < size; counter++)
+		array[counter] = c;
+	return array;
+}
