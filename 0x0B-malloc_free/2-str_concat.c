31dc5915e94a19c35291897ea33c8ea6b1187a02
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-13 19:36:15 +0000

    task 2 with refix

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * str_concat - concatenate given strings and assign a space
+ * @s1: the pointer to string
+ * @s2: the pointer to string
+ *
+ * Return: pointer to allocated space
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *array;
+	unsigned int counter, size1, size2;
+
+	size1 = 0;
+	size2 = 0;
+	counter = 0;
+	if (s1 == NULL && s2 == NULL)
+	{
+		array = malloc(sizeof(char));
+		array[0] = '\0';
+		return (array);
+	}
+	while (s1[counter])
+	{
+		size1++;
+		counter++;
+	}
+	counter = 0;
+	while (s2[counter])
+	{
+		size2++;
+		counter++;
+	}
+	counter = 0;
+	array = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (array == NULL)
+		return (NULL);
+	while (s1[counter])
+	{
+		array[counter] = s1[counter];
+		counter++;
+	}
+	counter = 0;
+	while (s2[counter])
+	{
+		array[size1 + counter] = s2[counter];
+		counter++;
+	}
+	array[size1 + size2] = '\0';
+	return (array);
+}
