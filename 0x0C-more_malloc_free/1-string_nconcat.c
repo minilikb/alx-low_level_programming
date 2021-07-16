3b577db0a86fcc0dd5d7b087c311db7d405cc9aa
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-16 19:24:08 +0000

    task 1 refis

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,6 +26,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		j = 0;
 	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = NULL;
 
 	address = malloc(sizeof(char *) * (j + i + 1));
 	if (address == NULL)
