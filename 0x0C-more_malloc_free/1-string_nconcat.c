f77b9fcb8d738c771ae62fcfb364c7efac860d17
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-16 19:21:18 +0000

    task 1 done

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * string-nconcat - concatenates selected bytes of string
+ * @s1: dest str
+ * @s2: src str
+ * @n: selected bytes
+ *
+ * Return: pointer to allocated space
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *address;
+	unsigned int i = 0, j = 0, c = 0, c2 = 0;
+
+	for (; s1[i]; i++)
+	{}
+	for (; s2[j]; j++)
+	{}
+	if (n < j)
+	{
+		j = n;
+	}
+	else
+	{
+		j = 0;
+	}
+
+	address = malloc(sizeof(char *) * (j + i + 1));
+	if (address == NULL)
+		return (NULL);
+
+	for (; c < i || c2 < j ; c++)
+	{
+		if (c < i)
+		{
+			address[c] = s1[c];
+		}
+		if( c >= i && c2 < j)
+		{
+			address[c] = s2[c2];
+			c2++;
+		}
+	}
+	address[c] = '\0';
+	return (address);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,6 +6,7 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
