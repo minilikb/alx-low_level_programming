4953683e31a622e98d75c3b0d1c5d1f7bdffb890
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-16 19:47:29 +0000

    refix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "holberton.h"
+
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - function that concat two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size of s2
+ * Return: void
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int c1 = 0;
+	unsigned int c2 = 0;
+	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[i] != '\0')
 	{
+		c1++;
+		i++;
 	}
-
-	for (j = 0; s2[j] != '\0'; j++)
+	while (s2[j] != '\0')
 	{
+		c2++;
+		j++;
 	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	if (c2 > n)
+		c2 = n;
+	str = malloc(sizeof(char) * (c1 + c2 + 1));
+	if (str == NULL)
+	{
 		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2];
-		co++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	for (i = 0; i < c1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < c2; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[c1 + c2] = '\0';
+	return (str);
 }
