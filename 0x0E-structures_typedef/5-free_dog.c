bcccf4b800a15e9f14582987126f1c5611b41d88
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-19 22:19:56 +0000

    task 5 done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - free a dog type
+ * @d: struct dog pointer
+ *
+ * Return: Always none
+ */
+void free_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
