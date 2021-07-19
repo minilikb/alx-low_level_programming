57eec7ff637b99a5e7f8760f21c44df82a37ad88
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-19 21:50:04 +0000

    task 3 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,13 @@ struct dog
 	float age;
 };
 
+/**
+ * typedef dot_t - new nick name
+ *
+ * Return: none
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
