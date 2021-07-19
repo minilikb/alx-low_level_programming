ace7a325e9fd224b55d4e107e38ac18989de0be5
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-19 20:46:59 +0000

    task 0 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef _DOG_
 #define _DOG_
 
-struct dog{
+/**
+ * struct dog - a struct type of dog
+ * @name: name of dog
+ * @owner: name of owner
+ * @age: age of dog
+ *
+ * Return: none
+ */
+
+struct dog
+{
 	char *name;
 	char *owner;
 	float age;
