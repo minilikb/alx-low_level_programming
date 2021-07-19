f5cbfdf723e66771d0b92ffe3183add45a5dbfda
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-19 21:08:03 +0000

    task 1 refix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * init_dog - init a dog type
  * @d: struct dog pointer
@@ -11,10 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
