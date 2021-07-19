49606d5abec9d8f95fa2021d5da442c893adf939
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-19 22:03:24 +0000

    task 4 refix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,10 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	if (name == NULL || owner == NULL)
-		return NULL;
+		return (NULL);
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-		return NULL;
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
