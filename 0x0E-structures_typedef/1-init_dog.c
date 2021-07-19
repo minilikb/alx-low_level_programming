95d74056b72c813190bd534eaa730303a831c3b0
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-19 21:00:33 +0000

    task 1 done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - init a dog type
+ * @d: struct dog pointer
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
