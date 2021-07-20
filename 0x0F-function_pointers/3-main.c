a3e7a6b6849dc22ae60174fdde942e042720bb04
Yohannes Terfa <lightshadow011@gmail.com>
2021-07-20 16:21:01 +0000

    task 4 refix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,12 +18,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (strlen(argv[2]) > 1)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op = get_op_func(argv[2]);
