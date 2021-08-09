9924ecd9acc017bb34197140b0b35c74d6129fbd
Yohannes Terfa <lightshadow011@gmail.com>
2021-08-09 19:14:20 +0000

    task 0 refix task mandatory done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,39 @@
 #include "holberton.h"
 
 /**
- * read_textfile - reads and prints a textfile
- * @filename: the name of the file
- * @letters: the size of letters to be read
- *
- * Return: -1 in failure and letters actually read
+ * read_textfile - Entry Point
+ * @filename: file name
+ * @letters: size
+ * Return: 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read_res, write_res;
-	char *buff;
+	int file, rd, wr;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY);
 
-	if (fd == -1)
-		return (0);
-	buff = malloc(sizeof(char) * letters + 1);
-	if (buff == NULL)
+	if (file == -1)
 		return (0);
 
-	read_res = read(fd, buff, letters);
-	if (read_res == -1)
-		return (0);
-	buff[letters] = '\0';
-	write_res = write(1, buff, read_res);
-	if (write_res == -1 || letters)
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
 		return (0);
 
-	close(fd);
-	free(buff);
-	return (write_res);
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
+
+	buf[letters] = '\0';
+
+	wr = write(1, buf, rd);
+	if (wr == -1)
+		return (0);
+
+	close(file);
+	free(buf);
+	return (wr);
 }
